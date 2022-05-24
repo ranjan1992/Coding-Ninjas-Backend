f10a5375850997adbf5b221b5ff7021ac022acc4
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-24 16:17:13 +0530

    2. Serving Response to Browser

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -1,6 +1,11 @@
 const http = require('http');
 const port = 8000;
-const server = http.createServer();
+
+function requestHandler(req, res) {
+  console.log(req.url);
+  res.end('Gotcha');
+}
+const server = http.createServer(requestHandler);
 
 server.listen(port, function (err) {
   if (err) {
