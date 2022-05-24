f0f106c35e743410c2181ebaf9a8e33f00f132d2
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-24 16:32:54 +0530

    3. Reading and Serving html from pc

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -1,9 +1,20 @@
 const http = require('http');
 const port = 8000;
+const fs = require('fs');
 
 function requestHandler(req, res) {
   console.log(req.url);
-  res.end('Gotcha');
+  res.writeHead(200, { 'content-type': 'text/html' });
+
+  fs.readFile('./index.html', function (err, data) {
+    if (err) {
+      console.log('error', err);
+      return res.end('<h1>Error !</h1>');
+    }
+    return res.end(data);
+  });
+
+  // res.end('<h1>Gotcha</h1>');
 }
 const server = http.createServer(requestHandler);
 
