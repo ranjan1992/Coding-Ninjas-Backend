dc4c0d8ca9be8f433de6fc9520b6718493a04627
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-24 18:08:00 +0530

    1. Setting up express server

---
+++ Contact_List/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log('Error in running the server ', err);
+  }
+  console.log('Yup ! Express Server is running on Port', port);
+});
