309d1a02dc2fec2faa7cfb18821123f1342af28f
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-26 15:48:51 +0530

    3. Creating view for home

--- codiel_major_project/index.js
+++ codiel_major_project/index.js
@@ -6,6 +6,10 @@ const port = 8001;
 
 app.use('/', require('./routes'));
 
+// Set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error in running the server: ${err}`);
