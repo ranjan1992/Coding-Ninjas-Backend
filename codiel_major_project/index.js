f4b1f96468ba4cea8ee027b345db65fa7f8fe1bd
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-26 15:16:20 +0530

    1. Create Router and Controller

--- codiel_major_project/index.js
+++ codiel_major_project/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8001;
+
+//Use the express router
+
+app.use('/', require('./routes'));
 
 app.listen(port, function (err) {
   if (err) {
