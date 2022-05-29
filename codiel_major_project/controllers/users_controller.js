92a6a0d4ae6abf2957b1eb1e8de3c131f6fa96d5
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-29 18:05:05 +0530

    17.  Passing User data to views and restricting page access

--- codiel_major_project/controllers/users_controller.js
+++ codiel_major_project/controllers/users_controller.js
@@ -5,10 +5,16 @@ module.exports.profile = function (req, res) {
 };
 
 module.exports.signUp = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/users/profile');
+  }
   return res.render('user_sign_up', { title: 'Codiel | Sign Up' });
 };
 
 module.exports.signIn = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/users/profile');
+  }
   return res.render('user_sign_in', {
     title: 'Codiel | SignIn',
   });
