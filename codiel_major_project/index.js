a6b6e625e02798ce4a07558eb33b481e17c3d29d
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-29 08:10:43 +0530

    12. About Cookies

--- codiel_major_project/index.js
+++ codiel_major_project/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 8001;
-const expressLayouts = require('express-ejs-Layouts');
+const cookieParser = require('cookie-parser');
+const port = 8000;
+
+const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 app.use(express.static('./assets'));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.use(expressLayouts);
 
