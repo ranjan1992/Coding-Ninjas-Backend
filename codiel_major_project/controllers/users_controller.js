9f5a1d8ff670d6137e8e262c64bb95a77d952527
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-29 10:18:05 +0530

    13. User Sign In

--- codiel_major_project/controllers/users_controller.js
+++ codiel_major_project/controllers/users_controller.js
@@ -38,5 +38,27 @@ module.exports.create = function (req, res) {
 };
 
 module.exports.createSession = function (req, res) {
-  //TO DO later
+  // Steps to authenticate
+
+  //find the user
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log('Error in finding user in Signing in');
+      return;
+    }
+
+    //handle user found
+    if (user) {
+      // handle password which don't match
+      if (user.password != req.body.password) {
+        return res.redirect('back');
+      }
+      //handle session creation
+      res.cookie('user_id', user.id);
+      return res.redirect('/users/profile');
+    } else {
+      //handle user not found
+      return res.redirect('back');
+    }
+  });
 };
