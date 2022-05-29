705e1b59c49ce84d05f931f8cb1856b9aee4e80d
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-29 09:50:00 +0530

    13. User SignUp

--- codiel_major_project/controllers/users_controller.js
+++ codiel_major_project/controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function (req, res) {
   return res.render('user_profile', { title: 'User Profile' });
 };
@@ -13,7 +15,26 @@ module.exports.signIn = function (req, res) {
 };
 
 module.exports.create = function (req, res) {
-  //To Do
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect('back');
+  }
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log('error in finding user in signing up');
+      return;
+    }
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log('Error in creating user while signing up');
+          return;
+        }
+        return res.redirect('/users/sign-in');
+      });
+    } else {
+      return res.redirect('back');
+    }
+  });
 };
 
 module.exports.createSession = function (req, res) {
