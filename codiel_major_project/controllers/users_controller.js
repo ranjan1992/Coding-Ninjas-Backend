fe56ad708de357b074bcbc42b848ba243f4b7a11
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-30 18:34:07 +0530

    31. Creating Flash Messages

--- codiel_major_project/controllers/users_controller.js
+++ codiel_major_project/controllers/users_controller.js
@@ -69,6 +69,7 @@ module.exports.create = function (req, res) {
 
 // sign in and create a session for the user
 module.exports.createSession = function (req, res) {
+  req.flash('success', 'Logged in Successfully');
   return res.redirect('/');
 };
 
@@ -77,6 +78,7 @@ module.exports.destroySession = function (req, res) {
     if (err) {
       return next(err);
     }
+    req.flash('success', 'You have logged out');
     res.redirect('/');
   });
 };
--- codiel_major_project/index.js
+++ codiel_major_project/index.js
@@ -10,6 +10,8 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMWare = require('./config/middleware');
 app.use(
   sassMiddleware({
     src: './assets/scss',
@@ -56,6 +58,8 @@ app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 //Use the express router
 
+app.use(flash());
+app.use(customMWare.setFlash);
 app.use('/', require('./routes'));
 
 // Set up the view engine
