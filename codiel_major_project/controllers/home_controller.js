7c7f97769457be10c62709cd4aa261f72da3f610
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-30 07:27:03 +0530

    23. Check Authentication on creating Posts

--- codiel_major_project/controllers/home_controller.js
+++ codiel_major_project/controllers/home_controller.js
@@ -13,6 +13,6 @@ module.exports.home = function (req, res) {
   Post.find({})
     .populate('user')
     .exec(function (err, posts) {
-      return res.render('home', { title: 'Codiel|Home', posts: posts });
+      return res.render('home', { title: 'Codeial|Home', posts: posts });
     });
 };
