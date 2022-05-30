50a80b79c6a9c08c8f659e25c49a20c678485a28
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-30 23:10:18 +0530

    34. Creating a Post ... Sending Data using AJAX

--- codiel_major_project/models/user.js
+++ codiel_major_project/models/user.js
@@ -1,25 +1,23 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-  {
+const userSchema = new mongoose.Schema({
     email: {
-      type: String,
-      required: true,
-      unique: true,
+        type: String,
+        required: true,
+        unique: true
     },
     password: {
-      type: String,
-      required: true,
+        type: String,
+        required: true
     },
     name: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timeStamps: true,
-  }
-);
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
 
 const User = mongoose.model('User', userSchema);
 
--- codiel_major_project/routes/users.js
+++ codiel_major_project/routes/users.js
@@ -1,30 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const usersController = require('../controllers/users_controller');
 const passport = require('passport');
 
-router.get(
-  '/profile/:id',
-  passport.checkAuthentication,
-  usersController.profile
-);
+const usersController = require('../controllers/users_controller');
 
-router.post(
-  '/update/:id',
-  passport.checkAuthentication,
-  usersController.update
-);
+router.get('/profile/:id', passport.checkAuthentication, usersController.profile);
+router.post('/update/:id', passport.checkAuthentication, usersController.update);
 
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 
+
 router.post('/create', usersController.create);
 
-router.post(
-  '/create-session',
-  passport.authenticate('local', { failureRedirect: '/users/sign-in' }),
-  usersController.createSession
-);
+// use passport as a middleware to authenticate
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'},
+), usersController.createSession);
+
 
 router.get('/sign-out', usersController.destroySession);
+
 module.exports = router;
