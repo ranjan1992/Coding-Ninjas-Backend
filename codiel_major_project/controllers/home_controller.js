117bc42b4c4c9944beb1236c23573f5881f75ef0
Ranjan Verma <verma.ranjan62@gmail.com>
2022-05-30 17:55:27 +0530

    30. Converting to Async -Await

--- codiel_major_project/controllers/home_controller.js
+++ codiel_major_project/controllers/home_controller.js
@@ -1,30 +1,24 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-module.exports.home = function (req, res) {
-  // console.log(req.cookies);
-  // res.cookie('codiel', 25);
-  // Post.find({}, function (err, posts) {
-  //   return res.render('home', {
-  //     title: 'Home Page',
-  //     posts: posts,
-  //   });
-  // });
-
-  Post.find({})
-    .populate('user')
-    .populate({
-      path: 'comments',
-      populate: {
-        path: 'user',
-      },
-    })
-    .exec(function (err, posts) {
-      User.find({}, function (err, users) {
-        return res.render('home', {
-          title: 'Codeial|Home',
-          posts: posts,
-          all_users: users,
-        });
+module.exports.home = async function (req, res) {
+  try {
+    let posts = await Post.find({})
+      .populate('user')
+      .populate({
+        path: 'comments',
+        populate: {
+          path: 'user',
+        },
       });
+
+    let users = await User.find({});
+
+    return res.render('home', {
+      title: 'Codeial|Home',
+      posts: posts,
+      all_users: users,
     });
+  } catch (err) {
+    console.log('Error', err);
+  }
 };
